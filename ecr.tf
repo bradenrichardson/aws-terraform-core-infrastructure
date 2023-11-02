c1975914dfe9b8b1ee7e3fae8ea3748ea5b11f86
Braden Richardson <braden.richardson13@gmail.com>
2023-11-02 09:03:41 +0800

    Update ecr.tf

--- ecr.tf
+++ ecr.tf
@@ -3,6 +3,24 @@ module "ecr" {
 
   repository_name = "private-streamlit-up"
 
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 30 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 30
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+
   tags = {
     Terraform   = "true"
     Environment = "dev"
